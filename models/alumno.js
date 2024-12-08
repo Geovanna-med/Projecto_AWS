48ea067e11caf5fee25a089258322eee881b3e52
Geovanna-med <73037185+Geovanna-med@users.noreply.github.com>
2024-12-08 17:11:16 -0600

    database

--- models/alumno.js
+++ models/alumno.js
@@ -1,11 +1,56 @@
-class Alumno {
-  constructor(id, nombres, apellidos, matricula, promedio) {
-    this.id = id;
-    this.nombres = nombres;
-    this.apellidos = apellidos;
-    this.matricula = matricula;
-    this.promedio = promedio;
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const Alumno = sequelize.define(
+  'Alumno',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombres: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apellidos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    matricula: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    promedio: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 10,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    fotoPerfilUrl: {
+      type: DataTypes.STRING,
+      allowNull: true, // Este campo es opcional
+    },
+  },
+  {
+    tableName: 'alumnos',
+    timestamps: true,
   }
-}
+);
 
 export default Alumno;
--- models/profesor.js
+++ models/profesor.js
@@ -1,11 +1,52 @@
-class Profesor {
-  constructor(id, nombres, apellidos, numeroEmpleado, horasClase) {
-    this.id = id;
-    this.nombres = nombres;
-    this.apellidos = apellidos;
-    this.numeroEmpleado = numeroEmpleado;
-    this.horasClase = horasClase;
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const Profesor = sequelize.define(
+  'Profesor',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombres: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[A-Za-z\s]+$/, // Solo permite letras y espacios
+        notEmpty: true,      // No permite valores vacíos
+      },
+    },
+    apellidos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[A-Za-z\s]+$/, // Solo permite letras y espacios
+        notEmpty: true,
+      },
+    },
+    numeroEmpleado: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isInt: true, // Solo acepta números enteros
+        min: 1,      // Mínimo valor permitido
+      },
+    },
+    horasClase: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+        min: 0, // No se permiten valores negativos
+      },
+    },
+  },
+  {
+    tableName: 'profesores', // Nombre de la tabla en la base de datos
+    timestamps: true, // Agrega campos createdAt y updatedAt automáticamente
   }
-}
+);
 
 export default Profesor;
